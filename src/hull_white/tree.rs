242c63c5716d38354fa7a0d01f36e98ef9e4b4da
jhashi19 <j.hashi19@gmail.com>
2024-02-18 01:19:01 +0900

    hull_white コメント修正・実装方針修正

--- src/hull_white/tree.rs
+++ src/hull_white/tree.rs
@@ -7,40 +7,30 @@ pub struct Tree {
     pub p_norm: Vec<Vec<f64>>, // 金利の通常の推移確率
     pub p_max: Vec<f64>,       // 金利方向の最大ノードの推移確率
     pub p_min: Vec<f64>,       // 金利方向の最小ノードの推移確率
-    pub base: Vec<Vec<f64>>,   // theta=0としたときのTree
+    pub tree: Vec<Vec<f64>>,   // Tree
 }
 
 impl Tree {
     // 初期処理
     // input: a, sigma, delta_t(ノードの時間間隔(横軸)), h(金利の間隔(縦軸)を計算するときのinput)
     // output: 金利の間隔の期待値, 金利の間隔の分散, 金利の間隔(縦軸)
-    // TODO インプットとアウトプットとして何が必要か整理できていないため整理する。
-    // TODO サンプルの実装なので、期間は10年固定でツリーを実装する、とか決めてしまってもよいかも。細かい実装の柔軟性は本質的ではない。
+    // TODO delta_tで時間幅を指定しているがTreeの満期と時間グリッドの数にした方がよい？
     pub fn new(a: f64, sigma: f64, delta_t: f64, h: f64) -> Self {
         let mean = (-a * delta_t).exp() - 1.0;
         let var = sigma.powi(2) * 0.5 / a * (1.0 - (-2.0 * a * delta_t).exp());
         let x_idx_max = ((1.0 - (1.0 - 1.0 / h).sqrt()) / mean).abs().ceil() as usize * 2;
         // p_normは(i, 0)がup、(i, 1)がmiddle、(i, 2)がdown
-        // インデックスを -n～n → 0～2n+1 に変換する必要がある。
+        // インデックスを -n～n → 0～2n に変換して推移確率を算出する。
         let mut p_norm = Vec::with_capacity(2 * x_idx_max + 1);
-        p_norm[x_idx_max + 1] = vec![0.5 * (1.0 / h), 1.0 - 1.0 / h, 0.5 * (1.0 / h)];
-        for i in 1..=x_idx_max {
-            // プラス側
-            let idx_mean = i as f64 * mean;
+        for i in 0..2 * x_idx_max + 1 {
+            let idx_change = i - x_idx_max;
+            let idx_mean = idx_change as f64 * mean;
             let prob_vec = vec![
                 0.5 * (1.0 / h + idx_mean.powi(2) + idx_mean),
                 1.0 - 1.0 / h - idx_mean.powi(2),
                 0.5 * (1.0 / h + idx_mean.powi(2) - idx_mean),
             ];
-            p_norm[x_idx_max + 1 + i] = prob_vec;
-            // マイナス側
-            let idx_mean_minus = -1.0 * i as f64 * mean;
-            let prob_vec = vec![
-                0.5 * (1.0 / h + idx_mean_minus.powi(2) + idx_mean_minus),
-                1.0 - 1.0 / h - idx_mean_minus.powi(2),
-                0.5 * (1.0 / h + idx_mean_minus.powi(2) - idx_mean_minus),
-            ];
-            p_norm[x_idx_max + 1 - i] = prob_vec;
+            p_norm[i] = prob_vec;
         }
 
         let idx_max_mean = x_idx_max as f64 * mean;
@@ -57,7 +47,7 @@ impl Tree {
             1.0 + 0.5 * (1.0 / h + idx_max_mean.powi(2) + 3.0 * idx_max_mean),
         ];
         let delta_x = (h * var).sqrt();
-        let base = Tree::construct_base_tree();
+        let tree = Tree::construct_tree();
         Tree {
             delta_t,
             delta_x,
@@ -67,21 +57,16 @@ impl Tree {
             p_norm,
             p_max,
             p_min,
-            base,
+            tree,
         }
     }
 
-    // サンプル
-    fn construct_base_tree() -> Vec<Vec<f64>> {
-        let mut base: Vec<Vec<f64>> = vec![vec![0.0; 10]; 10];
-        base
-    }
-
-    // theta=0 とした場合のツリーの構築
-    //   ノードごとの推移確率（Treeに持たせて初期処理で設定する方がよいかも）
-    //   ↑そこまでやってしまえばtheta=0の場合のツリーの構築までを初期処理でやる方がよいかも
-
+    // アロー・ドブリュー証券のツリーを作っておく？
     // 上記のツリーのイールドカーブへのフィッティング
+    fn construct_tree() -> Vec<Vec<f64>> {
+        let mut tree: Vec<Vec<f64>> = vec![vec![0.0; 10]; 10];
+        tree
+    }
 
     // 各種商品をバックワードでプライシング
 }
